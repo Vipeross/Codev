118788341919753221cd04906b3f7e166a5156f8
hartyshow <alexandre.frazer@etu.univ-lyon1.fr>
2018-11-23 17:51:19 +0100

    Ajout des ennemis + IA qui va le joueur + spawner aléatoire + tire des ennemis s'ils sont à portée

---
+++ Assets/EnnemyAI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnnemyAI : MonoBehaviour {
+
+    public GameObject weaponFireStart;
+    public GameObject weapon;
+    public GameObject bulletPrefab;
+
+    private float shootingRange = 5.0f;
+    private float fireRate = 1.0f;
+    private float timeSinceLastFire = 0.0f;
+    private GameObject bullet;
+
+    private GameObject objective;
+    private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        objective = GameObject.FindGameObjectWithTag("Player");
+
+        agent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        movement();
+        fire();
+    }
+
+    void movement ()
+    {
+        transform.LookAt(objective.GetComponent<Collider>().bounds.center);
+
+        if (Vector3.Distance(transform.position, objective.transform.position) < shootingRange)
+            agent.destination = transform.position;
+        else
+            agent.destination = objective.transform.position;
+    }
+
+    void fire ()
+    {
+        timeSinceLastFire += Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, objective.transform.position) < shootingRange && timeSinceLastFire >= fireRate)
+        {
+
+            Instantiate(bulletPrefab, weaponFireStart.transform.position, weapon.transform.rotation);
+
+            timeSinceLastFire = 0.0f;
+        }
+    }
+}
---
+++ Assets/GameManagement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class GameManagement : MonoBehaviour {
+
+    public GameObject EnnemyPrefab;
+
+    private float spawnFrequency = 10.0f;
+    private float timeSinceLastSpawn = 0.0f;
+    private int numberOfSpawns;
+
+	// Use this for initialization
+	void Start () {
+        numberOfSpawns = GameObject.FindGameObjectsWithTag("Respawn").Length;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        spawn();
+	}
+
+    void spawn()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if (timeSinceLastSpawn >= spawnFrequency)
+        {
+            int chosenSpawn = Random.Range(0, numberOfSpawns);
+
+            GameObject spawner = GameObject.FindGameObjectsWithTag("Respawn")[chosenSpawn];
+            Instantiate(EnnemyPrefab, spawner.transform.position, spawner.transform.rotation);
+
+            timeSinceLastSpawn = 0;
+        }
+    }
+}
