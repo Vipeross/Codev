ef57e97a980811e2ff97f45c5c52b9467941890f
Vipeross <jeremy.borghino@hotmail.com>
2018-11-23 11:02:20 +0100

    premier push

---
+++ Assets/shootBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootBehaviour : MonoBehaviour {
+
+    public GameObject weapon;
+    private float fireRate;
+    private float delay;
+
+
+	// Use this for initialization
+	void Start () {
+        fireRate = 1.0f;
+        delay = 0.0f;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        delay+= Time.deltaTime;
+        
+		if(Input.GetMouseButton(0) && delay > fireRate)
+        {
+            Debug.Log("ça tire");
+            delay = 0.0f;
+        }
+	}
+}
