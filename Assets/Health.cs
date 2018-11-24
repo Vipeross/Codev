ff81f4e34f2594e93191e56bd5b97540f68c1ddd
Yannis BACHA <yannis.bacha@hotmail.fr>
2018-11-24 13:31:32 +0100

    Généralisation Health + EnemyHealth

---
+++ Assets/Health.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	protected int startingHealth;
+	protected int currentHealth;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TakeDamage(int amount)
+	{
+		currentHealth -= amount;
+	}
+}
